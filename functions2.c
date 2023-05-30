d12710c67247b152d77b2a338cde47a6378b602a
abimbola19 <abimbolaonafowora@gmail.com>
2023-05-30 14:09:16 -0700

    bettying functions2.c

--- functions2.c
+++ functions2.c
@@ -135,6 +135,7 @@ int print_reverse(va_list types, char buffer[],
 	}
 	return (count);
 }
+
 /************************* PRINT A STRING IN ROT13 *************************/
 /**
  * print_rot13string - Print a string in rot13.
@@ -186,3 +187,4 @@ int print_rot13string(va_list types, char buffer[],
 	}
 	return (count);
 }
+
